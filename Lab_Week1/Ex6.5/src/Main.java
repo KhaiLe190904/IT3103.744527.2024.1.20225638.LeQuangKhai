821c50e592cae866c727f9dc4190938a34c48b25
KhaiLe190904 <lequangkhai190904@gmail.com>
2024-09-30 03:07:41 +0700

    final commit lab1

---
+++ Lab_Week1/Ex6.5/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Le Quang Khai - 20225638\nEnter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Le Quang Khai - 20225638\nEnter element of array: ");
+        int sum = 0;
+        for (int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+        Arrays.sort(arr);
+        System.out.println("Array after soft is: " + Arrays.toString(arr));
+        System.out.println("Sum of array is: " + sum);
+        System.out.println("Average of array is: " + (double) sum / arr.length);
+    }
+}
