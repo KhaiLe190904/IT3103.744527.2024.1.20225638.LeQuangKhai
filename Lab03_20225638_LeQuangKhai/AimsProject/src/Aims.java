82c98fff4b62f5d064134a67abbecd69ad153cfc
KhaiLe190904 <lequangkhai190904@gmail.com>
2024-11-22 22:28:12 +0700

    the exercise on method overloading

---
+++ Lab03_20225638_LeQuangKhai/AimsProject/src/Aims.java
@@ -0,0 +1,19 @@
+// Lê Quang Khải - 20225638
+
+public class Aims {
+    public static void main(String[] args) {
+        System.out.println("Lê Quang Khải - 20225638");
+        Cart anOrder = new Cart();
+        // Create new dvd objects and add them to the cart
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc(
+                "Your Name", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc(
+                "Smile", "Science Fiction", "George Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc(
+                "Animation", "Inside Out", 18.99f);
+//        DigitalVideoDisc[] dvdList = {dvd1, dvd2, dvd3};
+//        anOrder.addDigitalVideoDisc(dvdList);
+        anOrder.addDigitalVideoDisc(dvd1, dvd2);
+        anOrder.printAllDisc();
+    }
+}
